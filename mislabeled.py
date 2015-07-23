c742831e362665d79c787c035b1dca5cf911fb67
Israel Chauca Fuentes <israel@chauca.net>
2015-07-22 21:19:33 -0400

    More clean up and doc update.

--- mislabeled.py
+++ mislabeled.py
@@ -42,7 +42,7 @@ SCRIPT_DESC     = "You can put labels on anyone you want."
 
 OPTIONS         = {
                     'default_label'   : ('mislabeled', 'Text used to mark messages as mislabeled.'),
-                    'items'           : (''          , 'Space-separated regular expressions that will be matched against the nick!ident@host.item. Any user matching will get their message  marked as mislabeled. Can also include a comma-separated list of channels for every regular expression separated from the regexp by a colon. Prefix regexp with (?i) if you want it to be case insensitive. Example: "@\S+\.aol\.com$:#comcast,#AT&T (?i)!root@\S+" would mark messages in channels #comcast and #AT&T from users whose hosts end in *.aol.com, as well as all users who have any case variation of root as ident regardless of channel.'),
+                    'items'           : (''          , 'Space-separated regular expressions that will be matched against the nick!ident@host.item. Any user matching will get their message  marked as mislabeled. Can also include a comma-separated list of channels followed by a list of labels for every regular expression separated from each other and the regexp by a colon. Prefix regexp with (?i) if you want it to be case insensitive. Example: "@\S+\.aol\.com$:#comcast,#AT&T:trust (?i)!root@\S+ foobar::avoid" would label messages in channels #comcast and #AT&T from users whose hosts end in *.aol.com with "trust", all users who have any case variation of root as ident regardless of channel would be labeled with the default label; and any user with "foobar" on its nick or host in any channel with the label "avoid".'),
                     'separator'       : ('|'         , 'Character used to separate labels.'),
                     'delimiter_color' : ('cyan'      , 'Color used for the delimiters.'),
                     'label_color'     : ('red'       , 'Color used for labels.'),
@@ -51,17 +51,6 @@ DEBUG = False
 label_marker = '<mislabeled_marker:'
 label_marker_re = r'(%s)([^>]*)(>)' % label_marker
 
-# Easily use weechat colors in the script
-#   text = substitute_colors('my text ${color:yellow}yellow${color:default} colored.')
-# eval_expression():  to match ${color:nn} tags
-regex_color=re.compile('\$\{color:([^\{\}]+)\}')
-def substitute_colors(text):
-  global regex_color
-  if int(version) >= 0x00040200:
-    return weechat.string_eval_expression(text,{},{},{})
-  # substitute colors in output
-  return re.sub(regex_color, lambda match: weechat.color(match.group(1)), text)
-
 # ===================[ weechat options & description ]===================
 def init_options():
   for option,value in OPTIONS.items():
@@ -69,7 +58,8 @@ def init_options():
       weechat.config_set_plugin(option, value[0])
       toggle_refresh(None, 'plugins.var.python.' + SCRIPT_NAME + '.' + option, value[0])
     else:
-      toggle_refresh(None, 'plugins.var.python.' + SCRIPT_NAME + '.' + option, weechat.config_get_plugin(option))
+      toggle_refresh(None, 'plugins.var.python.' + SCRIPT_NAME + '.' + option, \
+          weechat.config_get_plugin(option))
     weechat.config_set_desc_plugin(option, '%s (default: "%s")' % (value[1], value[0]))
 
 def debug(str):
@@ -81,17 +71,19 @@ def update_items(items):
   mislabeled_items = []
   for item in items.split():
     pattern, chan, lbls = (item.split(':') + ['']*3)[:3]
-    channels = [channel.lower() for channel in chan.split(',') if channel != '']
-    labels = [label for label in lbls.split(',') if label != '']
+    channels = [channel.lower() for channel in chan.split(',') if channel]
+    labels = [label for label in lbls.split(',') if label]
     if not labels:
+      # At leat one label is required.
       labels.append(weechat.config_get_plugin('default_label'))
     mislabeled_items.append([pattern, re.compile(pattern), channels, labels])
-  debug('mislabeled items: %s' % str(mislabeled_items))
+  debug('updated items: %s' % str(mislabeled_items))
 
+# TODO remove this
 def toggle_refresh(pointer, name, value):
   global OPTIONS
-  option = name[len('plugins.var.python.' + SCRIPT_NAME + '.'):]        # get optionname
-  OPTIONS[option] = value                                               # save new value
+  option = name[len('plugins.var.python.' + SCRIPT_NAME + '.'):] # get optionname
+  OPTIONS[option] = value                                        # save new value
   if option == 'items':
     update_items(value)
   return weechat.WEECHAT_RC_OK
@@ -105,40 +97,42 @@ def mislabeled_cb(data, modifier, modifier_data, string):
   arguments = message_ht['arguments']
   channel = message_ht['channel']
 
-  new_arguments = re.sub(r'^%s :' % channel, '%s :%s' % (channel, label_marker + '>'), arguments)
-  # Move ACTION to its propper place.
-  new_arguments = re.sub(r"(<mislabeled_marker:>)(\x01ACTION )", r'\2\1', new_arguments)
+  # If it's a CTCP other than ACTION, do nothing.
+  debug(re.sub(r'\x01', 'x', arguments))
+  if not re.match(r'[^:]+:([^\x01]|\x01ACTION\b)', arguments):
+    return string
+  # Add the marker and keep ACTION on its propper place.
+  new_arguments = re.sub(r'^(%s :(\x01ACTION )?)' % channel, \
+      r'\1%s' % label_marker + '>', arguments)
   new_string = re.sub(r'%s$' % re.escape(arguments), new_arguments, string)
 
   for key, item_regexp, channels, labels in mislabeled_items:
-    debug('regexp is : %s' % item_regexp.pattern)
-    # If there is one or more channels listed for this item regexp, and none of them match the current channel, continue to the next mute item
+    # If there is one or more channels listed for this item regexp, and none of
+    # them match the current channel, continue to the next mute item.
     if len(channels) > 0 and channel.lower() not in channels:
       debug("%s doesn't match any of the listed channels: %s" % (channel, channels))
       continue
 
-    # If the hostitem matches the item regular expression, return the new, manipulated, string.
+    # If the hostitem matches the item regular expression, return the new,
+    # manipulated, string.
     debug("comparing %s to %s" % (item_regexp.pattern, hostitem))
     if item_regexp.search(hostitem):
       debug("  %s matches %s" % (item_regexp.pattern, hostitem))
-      #return new_string
-      debug("modifying: %s" % string)
-      debug("modified: %s" % new_string)
       [cur_labels.append(label) for label in labels if label not in cur_labels]
     if cur_labels:
-      debug('cur_list: %s' % ",".join(cur_labels))
+      # we have some tags to use
       string = re.sub(label_marker_re, r'\1\2%s\3' % ','.join(cur_labels), new_string)
-  # Nothing matches, so return the original, unmodified, string
   return string
 
 def colorize_cb(data, modifier, modifier_data, string):
   matches = re.search(r'<mislabeled_marker:([^>]*)>', string)
   if matches:
-    del_color = weechat.config_get_plugin('delimiter_color')
-    lbl_color = weechat.config_get_plugin('label_color')
-    sep = weechat.config_get_plugin('separator')
-    labels = (weechat.color(del_color) + sep + weechat.color(lbl_color)).join(matches.group(1).split(','))
-    string = re.sub(r'<mislabeled_marker:[^>]*>', '%s[%s%s%s]%s ' % (weechat.color("cyan"), weechat.color("red"), labels, weechat.color("cyan"), weechat.color("chat")), string)
+    delim_c = weechat.color(weechat.config_get_plugin('delimiter_color'))
+    lbl_c = weechat.color(weechat.config_get_plugin('label_color'))
+    sep = delim_c + weechat.config_get_plugin('separator') + lbl_c
+    labels = sep.join(matches.group(1).split(','))
+    string = re.sub(r'<mislabeled_marker:[^>]*>', '%s[%s%s%s]%s ' % \
+        (delim_c, lbl_c, labels, delim_c, weechat.color("chat")), string)
   return string
 
 def command_cb(data, buffer, args):
@@ -166,6 +160,6 @@ if __name__ == "__main__":
     hook = weechat.hook_modifier("irc_in_privmsg", "mislabeled_cb", "")
     hook = weechat.hook_modifier("weechat_print", "colorize_cb", "")
     hook = weechat.hook_command("mislabeled", "Add an item to the mislabeled list.",
-        "pattern[:channel1[,channel2[,...]]]",
-        "A pattern with an optional comma delimited list of channel names",
+        "pattern[:[channel,list][:label,list]]",
+        "A pattern, a colon, an optional comma delimited list of channel names, a colon and an optional comma delimited list of labels",
         "%(nick)", "command_cb", "")
